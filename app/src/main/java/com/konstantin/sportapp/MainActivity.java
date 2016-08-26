0781802bc09e0898c01ace58396b91ff11dc3e09
BaranovKM <irongrol@gmail.com>
2016-08-26 18:42:39 +0400

    Коммит остатков

---
+++ app/src/main/java/com/konstantin/sportapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.konstantin.sportapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//TODO найти/сделать иконки для кнопок(гантель для кнопки силовых, сердце для кнопки кардио и т.д.)
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
